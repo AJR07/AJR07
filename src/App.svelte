c09f3df425225359f597af8fce4a5d65a13ba685
AJR07 <angjunray@live.com>
2024-08-21 16:42:46 +0800

    :sparkles: Added an about me section

--- src/App.svelte
+++ src/App.svelte
@@ -4,35 +4,41 @@
 	import Navbar from './components/Navbar.svelte';
 </script>
 
-<main class="h-svh bg-black text-white">
+<main class="bg-black text-white">
 	<Navbar />
-	<div
-		id="main-content-container"
-		class="relative z-10 flex flex-col items-center"
-	>
-		<img
-			id="profile-picture"
-			src="AJR.png"
-			alt="AJR-profile"
-			class="-mb-[7.5%] -mt-[5%] w-3/4"
-		/>
-		<h1
-			id="header"
-			class="bg-gradient-to-r from-[#8eff65] to-[#1fd6ff] !bg-clip-text text-center text-[5vw] font-bold text-transparent [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]"
-		>
-			HALLOOOOOOOOO!
-		</h1>
-		<div>
-			<a class="px-5 text-[3vw] font-bold hover:underline" href="#about-me">
-				About Me
-			</a>
-			<span class="mx-10 text-[3vw] font-bold"> • </span>
-			<a class="px-5 text-[3vw] font-bold hover:underline" href="/projects">
-				Projects
-			</a>
+	<div id="container" class="mb-16">
+		<div id="main" class="flex min-h-svh flex-col items-center">
+			<img
+				id="profile-picture"
+				src="AJR.png"
+				alt="AJR-profile"
+				class="-mb-[7.5%] -mt-[5%] w-3/4"
+			/>
+			<h1
+				id="header"
+				class="bg-gradient-to-r from-[#8eff65] to-[#1fd6ff] !bg-clip-text text-center text-[5vw] font-bold text-transparent [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]"
+			>
+				HALLOOOOOOOOO!
+			</h1>
+			<div>
+				<a class="px-5 text-[3vw] font-bold hover:underline" href="#about-me">
+					About Me
+				</a>
+				<span class="mx-10 text-[3vw] font-bold"> • </span>
+				<a class="px-5 text-[3vw] font-bold hover:underline" href="/projects">
+					Projects
+				</a>
+			</div>
+		</div>
+
+		<div id="about-me">
+			<h1
+				id="header"
+				class="mx-10 bg-gradient-to-r from-[#46ffed] to-[#bd67ff] !bg-clip-text text-[4vw] font-bold text-transparent [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]"
+			>
+				> About Me
+			</h1>
 		</div>
 	</div>
 	<Footer />
 </main>
-
-<style></style>
