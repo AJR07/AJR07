ac940718be8b119a35f92a6bd69ecb107efefa02
AJR07 <angjunray@live.com>
2025-01-22 13:57:29 +0800

    :art: Simplify UI

--- src/pages/projects/Project.svelte
+++ src/pages/projects/Project.svelte
@@ -5,46 +5,53 @@
 
 	export let projectInfo: ProjectInfo;
 	export let chosenColour: ColourInfo;
+
+	let hovering = false;
 </script>
 
 <div
-	class="flex h-auto w-full transform flex-col gap-2 rounded-lg p-6 shadow-md transition duration-500 hover:scale-102"
+	class="flex h-40 flex-1 p-6 rounded-lg shadow-md transition duration-500 hover:scale-102"
 	style="{`background: ${chosenColour.overlay}; color: ${chosenColour.colour}`}"
+	role="region"
+	on:mouseenter="{() => (hovering = true)}"
+	on:mouseleave="{() => (hovering = false)}"
 >
-	<h3 class="text-3xl font-bold hover:cursor-pointer hover:underline">
-		<a href="{projectInfo.primaryLink}">
-			{projectInfo.title} v{projectInfo.version}
+	<div id="default-container" class="{`self-center transition duration-500 w-11/12 ${hovering ? 'opacity-0' : 'opacity-100'}`}">
+		<a
+			href="{projectInfo.primaryLink}"
+			class="text-3xl font-bold hover:underline"
+		>
+			{projectInfo.title}
 		</a>
-	</h3>
-
-	<!-- UPDATE TAILWIND.CONFIG.TS FOR DYNAMICALLY GENERATED STYLES -->
-	<div class="flex flex-row gap-2">
-		<span
-			class="inline-flex items-center rounded bg-{chosenColour.overall}-200 px-2 py-1 text-xs font-medium text-{chosenColour.overall}-900"
-		>
-			Created: {projectInfo.createdAt.toLocaleDateString()}
-		</span>
-		<span
-			class="inline-flex items-center rounded bg-{chosenColour.overall}-100 px-2 py-1 text-xs font-medium text-{chosenColour.overall}-800"
-		>
-			Last Updated: {projectInfo.lastUpdated.toLocaleDateString()}
-		</span>
 	</div>
-	<p>{projectInfo.description}</p>
 
-	<img src={projectInfo.img} alt={`project-${projectInfo.title}-image`}/>
-	<span class="flex-1"/>
-
-	<div id="links" class="flex flex-row gap-3">
-		{#each projectInfo.links as link}
-			<button
-				class="flex items-center gap-2 rounded px-4 py-2 font-bold text-white hover:scale-102"
-				style="{`background: ${chosenColour.overlay}`}"
-				on:click="{() => window.open(link.link, '_blank')}"
+	<div class="{`absolute transition duration-500 ${hovering ? 'opacity-100' : 'opacity-0'}`}">
+		<!-- UPDATE TAILWIND.CONFIG.TS FOR DYNAMICALLY GENERATED STYLES -->
+		<div class="flex flex-row gap-2">
+			<span
+				class="inline-flex items-center rounded bg-{chosenColour.overall}-200 px-2 py-1 text-xs font-medium text-{chosenColour.overall}-900"
 			>
-				<SquareArrowOutUpRight />
-				<span class="text-white">{link.name}</span>
-			</button>
-		{/each}
+				Created: {projectInfo.createdAt.toLocaleDateString()}
+			</span>
+			<span
+				class="inline-flex items-center rounded bg-{chosenColour.overall}-100 px-2 py-1 text-xs font-medium text-{chosenColour.overall}-800"
+			>
+				Last Updated: {projectInfo.lastUpdated.toLocaleDateString()}
+			</span>
+		</div>
+		<p>{projectInfo.description}</p>
+
+		<div id="links" class="flex flex-row gap-3">
+			{#each projectInfo.links as link}
+				<button
+					class="flex items-center gap-2 rounded px-4 py-2 font-bold text-white hover:scale-102"
+					style="{`background: ${chosenColour.overlay}`}"
+					on:click="{() => window.open(link.link, '_blank')}"
+				>
+					<SquareArrowOutUpRight />
+					<span class="text-white">{link.name}</span>
+				</button>
+			{/each}
+		</div>
 	</div>
 </div>
