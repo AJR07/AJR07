d13a12fad366ebbc2adf13627d77b9b8ecd1e599
AJR07 <angjunray@live.com>
2025-01-23 09:02:17 +0800

    :sparkles: Add resume

--- src/App.svelte
+++ src/App.svelte
@@ -6,10 +6,12 @@
 	import Footer from './components/Footer.svelte';
 	import NotFound from './pages/NotFound.svelte';
 	import Projects from './pages/Projects.svelte';
+	import Resume from './pages/Resume.svelte';
 
 	const routes = {
 		'/': Home,
 		'/projects': Projects,
+		'/resume': Resume,
 		'*': NotFound
 	};
 </script>
---
+++ src/pages/Resume.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Generator from './generator/Generator.svelte';
+	import projectContainers from '../data/projects.json';
+	import type { Colour } from 'src/types/Colours';
+
+	// Project Containers
+	const processedProjectContainers = projectContainers.map((container) => ({
+		...container,
+		colourChoice: container.colourChoice as Colour
+	}));
+</script>
+
+<Generator projectContainers={processedProjectContainers}>
+	<img
+		id="profile-picture"
+		src="AJR.png"
+		alt="AJR-profile"
+		class="-mb-[7.5%] -mt-[5%] w-3/4"
+	/>
+	<h1
+		id="header"
+		class="bg-gradient-to-r from-[#58ff5d] to-[#ffd000] !bg-clip-text text-[5vw] font-bold text-transparent [-webkit-background-clip:text] [-webkit-text-fill-color:transparent]"
+	>
+		My Resume
+	</h1>
+</Generator>
