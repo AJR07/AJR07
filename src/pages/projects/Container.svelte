1141b45203e9dd49d445c293bc723d4eca6c7ba5
AJR07 <angjunray@live.com>
2025-01-24 13:12:09 +0800

    :sparkles: Done with main page!!

--- src/pages/projects/Container.svelte
+++ src/pages/projects/Container.svelte
@@ -11,7 +11,7 @@
 	const chosenColour = COLOURS[colourChoice];
 </script>
 
-<div id={containerTitle} class="p-6 pl-28 h-min" style="{`background: ${chosenColour.background}`}">
+<div id={containerTitle} class="p-6 pl-20 h-min" style="{`background: ${chosenColour.background}`}">
 	<h2
 		class="text-5xl font-bold mb-6"
 		style="{`color: ${chosenColour.colour}`}"
