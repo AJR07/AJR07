a345e17f4f1486ce33aaf189f4d4f34c9d45d254
AJR07 <angjunray@live.com>
2025-01-24 09:09:07 +0800

    :tada: Complete AboutMe

--- src/pages/aboutme/AboutMe.svelte
+++ src/pages/aboutme/AboutMe.svelte
@@ -6,8 +6,38 @@
 
 	<h3 class="mt-5 text-3xl font-bold">Preferred Tech Stack</h3>
 
-	<div id="languages" class="flex flex-row gap-3 my-1 p-3 h-14 bg-green-700/20 duration-500 tranistion-all hover:scale-102">
-		<h4 class="text-xl font-semibold">Programming Languages: </h4>
+	<div
+		id="others"
+		class="tranistion-all my-1 flex h-14 flex-row gap-3 bg-yellow-700/30 p-3 duration-500 hover:scale-102"
+	>
+		<h4 class="text-xl font-semibold">General:</h4>
+		<img
+			src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/windows.png"
+			alt="Windows"
+		/>
+		<img
+			src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/arch_linux.png"
+			alt="Arch Linux"
+		/>
+		<img
+			src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/visual_studio_code.png"
+			alt="VS Code"
+		/>
+		<img
+			src="https://raw.githubusercontent.com/onemarc/tech-icons/refs/heads/main/icons/blender-dark.svg"
+			alt="Blender"
+		/>
+		<img
+			src="https://private-user-images.githubusercontent.com/172177048/375818234-fb6070fb-3590-4985-8662-0b1d32c40325.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzc2ODEwMDgsIm5iZiI6MTczNzY4MDcwOCwicGF0aCI6Ii8xNzIxNzcwNDgvMzc1ODE4MjM0LWZiNjA3MGZiLTM1OTAtNDk4NS04NjYyLTBiMWQzMmM0MDMyNS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMTI0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDEyNFQwMTA1MDhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00MTYyYmIzMDVkZmFkNDlmMzE3ODE0NmE5OThhMDQ1ZjRmMDkxYzVlMTU2NTZlMzUxODQ4NDY0YTViY2UxNzM2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.i2kkx6UjXDAQM2kX7WT7Xf9jzZgjQmz3LYPY2lPT61w"
+			alt="Davinci Resolve"
+		/>
+	</div>
+
+	<div
+		id="languages"
+		class="tranistion-all my-1 flex h-14 flex-row gap-3 bg-green-700/30 p-3 duration-500 hover:scale-102"
+	>
+		<h4 class="text-xl font-semibold">Programming Languages:</h4>
 		<img
 			src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/python.png"
 			alt="Python"
@@ -30,8 +60,11 @@
 		/>
 	</div>
 
-	<div id="web-dev" class="flex flex-row gap-3 my-1 p-3 h-14 bg-blue-700/20 duration-500 tranistion-all hover:scale-102">
-		<h4 class="text-xl font-semibold">Web Dev: </h4>
+	<div
+		id="web-dev"
+		class="tranistion-all my-1 flex h-14 flex-row gap-3 bg-blue-700/30 p-3 duration-500 hover:scale-102"
+	>
+		<h4 class="text-xl font-semibold">Web Dev:</h4>
 		<img
 			src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/visual_studio_code.png"
 			alt="VS Code"
