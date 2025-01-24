3a81007a0e2cee7692654be67f850a5388a79e52
AJR07 <angjunray@live.com>
2025-01-24 09:47:35 +0800

    :sparkles: Almost done with AboutMe

---
+++ src/pages/aboutme/PhotoCarousel.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<h3 class="mt-5 text-3xl font-bold">Cool Photos of Planes</h3>
---
+++ src/pages/aboutme/TechStack.svelte
@@ -0,0 +1,94 @@
+<h3 class="mt-5 text-3xl font-bold">Preferred Tech Stack</h3>
+
+<div
+	id="others"
+	class="tranistion-all my-1 flex h-14 flex-row gap-3 bg-yellow-700/30 p-3 duration-500 hover:scale-102"
+>
+	<h4 class="text-xl font-semibold">General:</h4>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/windows.png"
+		alt="Windows"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/arch_linux.png"
+		alt="Arch Linux"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/visual_studio_code.png"
+		alt="VS Code"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/onemarc/tech-icons/refs/heads/main/icons/blender-dark.svg"
+		alt="Blender"
+	/>
+	<img
+		src="https://private-user-images.githubusercontent.com/172177048/375818234-fb6070fb-3590-4985-8662-0b1d32c40325.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzc2ODEwMDgsIm5iZiI6MTczNzY4MDcwOCwicGF0aCI6Ii8xNzIxNzcwNDgvMzc1ODE4MjM0LWZiNjA3MGZiLTM1OTAtNDk4NS04NjYyLTBiMWQzMmM0MDMyNS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMTI0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDEyNFQwMTA1MDhaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00MTYyYmIzMDVkZmFkNDlmMzE3ODE0NmE5OThhMDQ1ZjRmMDkxYzVlMTU2NTZlMzUxODQ4NDY0YTViY2UxNzM2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.i2kkx6UjXDAQM2kX7WT7Xf9jzZgjQmz3LYPY2lPT61w"
+		alt="Davinci Resolve"
+	/>
+</div>
+
+<div
+	id="languages"
+	class="tranistion-all my-1 flex h-14 flex-row gap-3 bg-green-700/30 p-3 duration-500 hover:scale-102"
+>
+	<h4 class="text-xl font-semibold">Programming Languages:</h4>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/python.png"
+		alt="Python"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/javascript.png"
+		alt="JavaScript"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/typescript.png"
+		alt="TypeScript"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/swift.png"
+		alt="Swift"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/c++.png"
+		alt="C++"
+	/>
+</div>
+
+<div
+	id="web-dev"
+	class="tranistion-all my-1 flex h-14 flex-row gap-3 bg-blue-700/30 p-3 duration-500 hover:scale-102"
+>
+	<h4 class="text-xl font-semibold">Web Dev:</h4>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/visual_studio_code.png"
+		alt="VS Code"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/typescript.png"
+		alt="TypeScript"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/vite.png"
+		alt="Vite"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/react.png"
+		alt="React"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/svelte.png"
+		alt="Svelte"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/tailwind_css.png"
+		alt="Tailwind CSS"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/firebase.png"
+		alt="Firebase"
+	/>
+	<img
+		src="https://raw.githubusercontent.com/marwin1991/profile-technology-icons/refs/heads/main/icons/supabase.png"
+		alt="Supabase"
+	/>
+</div>
